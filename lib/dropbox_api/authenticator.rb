52a53d76b988ea955579294f4c3e43cc0fe47ec3
Alex VanRielly <aorielly@gmail.com>
2026-06-11 10:24:23 +0200

    Upgrading the dropbox_api oauth2 requirements

--- lib/dropbox_api/authenticator.rb
+++ lib/dropbox_api/authenticator.rb
@@ -6,7 +6,10 @@ module DropboxApi
     def initialize(client_id, client_secret)
       super(client_id, client_secret, {
         authorize_url: 'https://www.dropbox.com/oauth2/authorize',
-        token_url: 'https://api.dropboxapi.com/oauth2/token'
+        token_url: 'https://api.dropboxapi.com/oauth2/token',
+        # oauth2 2.x defaults to :basic_auth; Dropbox accepts both, but
+        # :request_body preserves the wire format this gem has always used.
+        auth_scheme: :request_body
       })
     end
   end
